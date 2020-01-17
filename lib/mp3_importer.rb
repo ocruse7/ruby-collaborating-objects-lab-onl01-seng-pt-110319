87557bebf5d6d4c560b59c197abb85164540eda1
Owen Cruse <ocruse7@gmail.com>
2020-01-17 22:41:23 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+  
+  attr_accessor :path
+  
+  def initialize(filePath)
+    @path = filePath
+  end
+  
+  def files 
+    Dir.glob("#{@path}/*.mp3").map {|file| file.gsub("#{@path}/","")}
+  end
+  
+  def import
+    self.files.each {|file| Song.new_by_filename(file)}
+  end
+
+end
